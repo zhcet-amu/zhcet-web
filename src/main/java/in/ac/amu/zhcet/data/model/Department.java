b56df66d1ecfa6f6482a346d8d4653a444c61064
Divy <divs4debu@gmail.com>
2017-06-18 10:09:42 +0530

    feat: Add department model

---
+++ src/main/java/in/ac/amu/zhcet/data/model/Department.java
@@ -0,0 +1,37 @@
+package in.ac.amu.zhcet.data.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Entity
+public class Department {
+
+    @Column(unique = true)
+    private String departmentId;
+
+    private String departmentName;
+
+    public Department() {
+    }
+
+    public Department(String departmentId, String departmentName) {
+        this.departmentId = departmentId;
+        this.departmentName = departmentName;
+    }
+
+    public String getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(String departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+}
