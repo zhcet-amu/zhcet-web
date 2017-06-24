ebeabb44ee9581dc2785f53fd3f33e73064135f3
Divy <divs4debu@gmail.com>
2017-06-25 00:37:33 +0530

    refac: Add checks in dean-controller

--- src/main/java/in/ac/amu/zhcet/controller/DeanController.java
+++ src/main/java/in/ac/amu/zhcet/controller/DeanController.java
@@ -1,16 +1,28 @@
 package in.ac.amu.zhcet.controller;
 
+import in.ac.amu.zhcet.data.model.Student;
 import in.ac.amu.zhcet.data.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class DeanController {
     @GetMapping("/dean-admin")
     public String deanAdmin(Model model){
         model.addAttribute("user", new User());
+        return "user";
+    }
+
+    @PostMapping("/dean-admin")
+    public String enterUser(Model model, @ModelAttribute("user") User user, BindingResult bindingResult, @RequestParam String enrol){
+        String role = user.getRoles()[0];
+        if(role.equals("ROLE_STUDENT")) {
+            Student student = new Student(user, enrol);
+        }else if(role.equals("ROLE_FACULTY")){
+
+        }
         return "user";
     }
 }
