c21a75898f3697338da01d9903855766013a1b2b
Divy <divs4debu@gmail.com>
2017-07-28 11:03:26 +0530

    fix: Unsed imports removed

--- src/main/java/in/ac/amu/zhcet/controller/FileUploadController.java
+++ src/main/java/in/ac/amu/zhcet/controller/FileUploadController.java
@@ -1,16 +1,10 @@
 package in.ac.amu.zhcet.controller;
-
-import org.apache.tomcat.util.http.fileupload.FileUpload;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 
 @Controller
