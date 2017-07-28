04c8a9d84bd8e628c431e6fb3ff4a49eb0007cdb
Areeb Jamal <jamal.areeb@gmail.com>
2017-07-28 13:33:02 +0530

    fix: Upload attendance to correct endpoint

--- src/main/java/in/ac/amu/zhcet/controller/FacultyController.java
+++ src/main/java/in/ac/amu/zhcet/controller/FacultyController.java
@@ -8,7 +8,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @Controller
@@ -38,5 +42,20 @@ public class FacultyController {
         List<CourseRegistration> courseRegistrations = floatedCourse.getCourseRegistrations();
         model.addAttribute("courseRegistrations", courseRegistrations);
         return "course_attendance";
+    }
+
+    @PostMapping("faculty/courses/{id}/attendance")
+    public String uploadFile(Model model, @PathVariable String id, @RequestParam("file") MultipartFile file) throws IOException {
+        if (!file.isEmpty()) {
+            try {
+                byte[] bytes = file.getBytes();
+                String completeData = new String(bytes);
+                System.out.print(completeData);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        return "redirect:/faculty/courses/{id}";
     }
 }
