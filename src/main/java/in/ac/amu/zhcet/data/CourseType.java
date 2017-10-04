5cd1c624386e17b15702fbf6563bdb1969ff4291
iamareebjamal <jamal.areeb@gmail.com>
2017-10-04 16:27:44 +0530

    feat: Add create course page

--- src/main/java/in/ac/amu/zhcet/data/CourseType.java
+++ src/main/java/in/ac/amu/zhcet/data/CourseType.java
@@ -3,4 +3,17 @@ package in.ac.amu.zhcet.data;
 public enum CourseType {
     T, // Theory
     P  // Practical
+    ;
+
+    @Override
+    public String toString() {
+        switch (this) {
+            case T:
+                return "Theory";
+            case P:
+                return "Practical";
+            default:
+                return "Theory";
+        }
+    }
 }
