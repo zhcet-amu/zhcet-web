30a925f097c4760ced6476c8b074aa67265e5e54
Areeb Jamal <jamal.areeb@gmail.com>
2018-10-19 11:09:44 +0530

    fix: Resource versioning content strategy
    - Add workaround for issue https://github.com/spring-projects/spring-boot/issues/14897

--- src/main/java/amu/zhcet/CoreApplication.java
+++ src/main/java/amu/zhcet/CoreApplication.java
@@ -8,8 +8,13 @@ import io.sentry.Sentry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.datatables.repository.DataTablesRepositoryFactoryBean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.resource.ResourceUrlEncodingFilter;
+
+import javax.servlet.DispatcherType;
 
 @SpringBootApplication
 @EnableJpaRepositories(repositoryFactoryBeanClass = DataTablesRepositoryFactoryBean.class)
@@ -20,6 +25,15 @@ public class CoreApplication {
     public static void main(String[] args) {
         Sentry.init();
         SpringApplication.run(CoreApplication.class, args);
+    }
+
+    // Workaround of issue https://github.com/spring-projects/spring-boot/issues/14897
+    // Remove when fix is released
+    @Bean
+    public FilterRegistrationBean<ResourceUrlEncodingFilter> resourceUrlEncodingFilter() {
+        FilterRegistrationBean<ResourceUrlEncodingFilter> registration = new FilterRegistrationBean<>(new ResourceUrlEncodingFilter());
+        registration.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ERROR);
+        return registration;
     }
 
 }
