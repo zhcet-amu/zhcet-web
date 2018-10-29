ec840b6cb1a0a34e06131acf5707bc1df32c6980
Areeb Jamal <jamal.areeb@gmail.com>
2018-10-30 03:03:42 +0530

    feat: Add workbox

--- src/main/resources/src/app.mjs
+++ src/main/resources/src/app.mjs
@@ -4,3 +4,9 @@ if (!!window.toastr) {
     toastr.options.progressBar = true;
     toastr.options.closeButton = true;
 }
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js');
+    });
+}
