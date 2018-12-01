b57a32f0255d8b42657f3499b8b75375c6fa36f8
Areeb Jamal <jamal.areeb@gmail.com>
2018-12-02 04:13:58 +0530

    fix: Idea JS generation (#277)

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,10 @@ import fs from 'fs-extra';
 import buble from 'rollup-plugin-buble';
 import { terser } from 'rollup-plugin-terser';
 import resolve from 'rollup-plugin-node-resolve';
-import postcss from 'rollup-plugin-postcss'
+import postcss from 'rollup-plugin-postcss';
+import replace from 'rollup-plugin-replace';
+
+require('dotenv').config();
 
 const isProdBuild = process.env.NODE_ENV === 'production';
 const resourcePath = path.resolve(__dirname, 'src', 'main', 'resources');
@@ -25,37 +28,77 @@ function getModuleName(file) {
     return fileName.replace(/-/g, '_');
 }
 
+function getOutputFileContent(
+    outputFileName,
+    outputFile,
+    outputOptions
+) {
+    if (typeof outputFile.code === 'string') {
+        let source =  outputFile.code;
+        if (outputOptions.sourcemap && outputFile.map) {
+            const url =
+                outputOptions.sourcemap === 'inline'
+                    ? outputFile.map.toUrl()
+                    : `${path.basename(outputFileName)}.map`;
+
+            // https://github.com/rollup/rollup/blob/master/src/utils/sourceMappingURL.ts#L1
+            source += `//# source` + `MappingURL=${url}\n`
+        }
+        return source
+    } else {
+        return outputFile
+    }
+}
+
 function copy() {
     return {
         name: 'copy',
         generateBundle(outputOptions, bundle, isWrite) {
-            if (isWrite) {
-                const relativePath = path.relative(resourcePath, outputOptions.file);
-                const ideaOutputPath = path.resolve(ideaResourcePath, relativePath);
-                return fs.copy(outputOptions.file, ideaOutputPath);
-            }
+            if (!isWrite)
+                return;
+
+            const relativePath = path.relative(resourcePath, outputOptions.file);
+            const ideaOutputPath = path.resolve(ideaResourcePath, relativePath);
+
+            const directory = path.dirname(ideaOutputPath);
+
+            return Promise.all(Object.keys(bundle).map(async fileName => {
+                const fileEntry = bundle[fileName];
+
+                const fileContent = getOutputFileContent(
+                    fileName,
+                    fileEntry,
+                    outputOptions,
+                );
+
+                await fs.mkdirp(directory);
+                await fs.writeFile(ideaOutputPath, fileContent);
+            }));
         }
     }
 }
 
 async function getConfig() {
     const paths = await globby(sourcePath);
-    const commonPlugins = [
+    const plugins = [
         postcss({
             minimize: isProdBuild,
             plugins: []
         }),
+        buble({
+            objectAssign: 'Object.assign',
+            jsx: 'h'
+        }),
         resolve(),
+        replace({
+            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+        }),
         copy()
     ];
-    const plugins = isProdBuild ?
-        [
-            ...commonPlugins,
-            buble({
-                objectAssign: 'Object.assign'
-            }),
-            terser()
-        ] : commonPlugins;
+
+    if (isProdBuild) {
+        plugins.push(terser());
+    }
 
     return paths.map(file => ({
         input: file,
